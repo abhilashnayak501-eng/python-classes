71f5961700c360f6ee988adc590ef6c17c8a704f
Abhilash Nayak <abhilashnayak501@gmail.com>
2026-09-17 11:07:12 +0530

    Restore and update string manipulation examples

--- postive indexing.py
+++ postive indexing.py
@@ -1,29 +1,29 @@
-# str = "Abhilash nayak"
-# # print(str[-2:-1])
-# # print(str[::-1])
-# # print(str[-1:-6:-1])
-# del str[-1:-5:2]
-# print(str)
-# str = 'abhialsh'
-# print(len(str+ " " +"nayak"))
-# print(str)
-# 
-# str = "Abhilash Nayak"
-# # print(max(str))
-# # print(min(str))
-# # print(len(str))
-# # print(sorted(str,reverse=True))
-# print(str.capitalize())
-# print(str.title())
-# print(str.upper())
-# print(str.lower())
-# print(str.swapcase())
-# print(str.count("a"))
-# print(str.find("a"))
-# name = "Abhilash"
-# age = "23"
+str = "Abhilash nayak"
+print(str[-2:-1])
+print(str[::-1])
+print(str[-1:-6:-1])
+del str[-1:-5:2]
+print(str)
+str = 'abhialsh'
+print(len(str+ " " +"nayak"))
+print(str)
 
-# print("my name is {} and my age is {}".format(name,age))
+str = "Abhilash Nayak"
+print(max(str))
+print(min(str))
+print(len(str))
+print(sorted(str,reverse=True))
+print(str.capitalize())
+print(str.title())
+print(str.upper())
+print(str.lower())
+print(str.swapcase())
+print(str.count("a"))
+print(str.find("a"))
+name = "Abhilash"
+age = "23"
+
+print("my name is {} and my age is {}".format(name,age))
 s = "abhilash is my name and my age is 23"
 print(s.split(" "))
 print(s.join("-"))
