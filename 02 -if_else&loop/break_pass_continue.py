5d9dea1e605795f7050acf82c73978574c20a76c
Abhi <abhilashnayak501@gmail.com>
2026-09-23 20:53:54 +0530

    string's sliceing concept done

--- 02 -if_else&loop/break_pass_continue.py
+++ 02 -if_else&loop/break_pass_continue.py
@@ -2,9 +2,12 @@ for i in range(1,100):
     if i == 10:
         break
     print(i) 
-# # example of continue satement
-# for i in range(1,10):
-#     if i == 6:
-#         continue
-#     print(i)                      
+# example of continue satement
+for i in range(1,10):
+    if i == 6:
+        continue
+    print(i)
+# use cases of pass statment
+for i in range(1,10):
+    pass
 
--- strings/string_practice_qs.py
+++ strings/string_practice_qs.py
@@ -42,6 +42,87 @@ print(s[0::2])
 s = "hello_anhi"
 print(s[7:0:-1])
 # always remember negative indexing is always start with higher no and lower no
+# # this programing code for revers the entire string
 s = "hello_abhi"
 print(s[::-1])
+# output is ihba_olleh
+# Neagtive stepsize in string
+s = "HELLO_WORLD"
+print(s[10:2:-1])
+# output is DLROW_OL
+# print only world
+s = "hello_world"
+print(s[-5:])
+s = "hello_world"
+print(s[-1:-11:-2])
+
+word = "PROGRAMMING"
+
+# Question 1: Positive Slicing
+# Predict the output of: word[3:8]
+print(word[3:8])
+
+# Question 2: Negative Slicing
+# Predict the output of: word[-7:-3]
+print(word[-7:-3])
+
+# Question 3: Step Size Slicing
+# Predict the output of: word[1:9:3]
+print(word[1:9:3])
+
+# Question 4: Negative Slicing with Missing Bounds (End tak jana)
+# Predict the output of: word[-5:]
+print(word[-5:])
+
+# Question 5: Advanced Reverse Slicing with Step
+# Predict the output of: word[::-2]
+print(word[::-2])
+word = "ELECTRONICS"
+
+# Question 1: Basic Negative Slicing
+# Predict the output of: word[-9:-4]
+print("Q1:", word[-9:-4])
+
+# Question 2: Negative Slicing till the End (Stop missing)
+# Predict the output of: word[-5:]
+print("Q2:", word[-5:])
+
+# Question 3: Negative Slicing from the Start (Start missing)
+# Predict the output of: word[:-6]
+print("Q3:", word[:-6])
+
+# Question 4: Negative Slicing with Positive Step Size
+# Predict the output of: word[-10:-2:2]
+print("Q4:", word[-10:-2:2])
+
+# Question 5: Negative Slicing with Negative Step Size (Reverse direction)
+# Predict the output of: word[-2:-8:-1]
+print("Q5:", word[-2:-8:-1])
+word = "BERHAMPUR"
+
+# Question 1: Positive Start, Negative Stop
+# Predict the output of: word[2:-2]
+print("Q1:", word[2:-2])
+
+# Question 2: Negative Start, Positive Stop with Step
+# Predict the output of: word[-8:6:2]
+print("Q2:", word[-8:6:2])
+
+# Question 3: Reverse Slicing with specific bounds
+# Predict the output of: word[-2:2:-1]
+print("Q3:", word[-2:2:-1])
+
+# Question 4: Missing bounds with a Negative Step
+# Predict the output of: word[::-3]
+print("Q4:", word[::-3])
+file = input("Enter the file name ->")
+new_file = file[-3:]
+print(new_file)
+file = input("Enter the year file ->")
+new_year_file = file[-8:-4]
+print(new_year_file)
+s = "Abhilash_Nayak"
+del s
+print(s)
+
 
