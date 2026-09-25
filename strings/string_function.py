66f49b191c838b91208dbf226cfd37365f5198c1
Abhi <abhilashnayak501@gmail.com>
2026-09-25 19:01:33 +0530

    learning some str_function

---
+++ strings/string_function.py
@@ -0,0 +1,34 @@
+# common_function
+# Length functions
+s = "Abhilash_Nayak" 
+print(len(s))
+# Maximun functions
+s = "Abhilash_Nayak"
+print(max(s))
+# Minimum functions
+s = "Abhilash_Nayak"
+print(min(s))
+# Sorted functions
+s = "Abhilash_Nayak"
+print(sorted(s,reverse=True))
+# Only work in strings functions
+# Capitalize
+# Titel
+# Upper
+# Lower
+# swapcase
+# Capitalize ->Converts the very first character of the string to uppercase and converts all other characters to lowercase.
+s = "abhilash"
+print(s.capitalize())
+#Titel->Converts the first letter of each word to uppercase and all remaining letters in each word to lowercase.
+s = "abhilash nayak"
+print(s.title())
+# lower->
+s = "Abhilash"
+print(s.lower())
+# swapcase->
+s = "Abhilash"
+print(s.swapcase())
+# upper
+s = "abhilash"
+print(s.upper())
